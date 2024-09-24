2b19eeaf35fef4b415a4697403fdb08997e4f013
Max Kalchenko <maxim.kalchenko@tech-stack.io>
2024-09-24 16:01:25 +0200

    Modify the output configuration removal

--- mf-ex-client-angular/extra-webpack.config.js
+++ mf-ex-client-angular/extra-webpack.config.js
@@ -3,9 +3,6 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
-  // Modify the output configuration
-  singleSpaWebpackConfig.output.libraryTarget = 'system';
-
   // Set up externals
   singleSpaWebpackConfig.externals = {
     '@maxkalchenko/utils': '@maxkalchenko/utils', // Mark as external
